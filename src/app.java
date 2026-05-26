ffe52c4d771e6ed2c2f33ea335ff2899069f2a0c
Baptou302 <kikijeamichelle@gmail.com>
2026-05-26 14:28:10 +0200

    EE

--- src/app.java
+++ src/app.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Blackcjack blackjack = new Blackcjack();
+        BlackJack blackJack = new BlackJack();
     }
 }
